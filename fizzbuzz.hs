5df7787e28c7cc1e2d53fcda58d27ca4608613c7
michaelrauh <michaelrauh@ymail.com>
2019-01-05 12:34:38 -0500

    use guards instead of if to implement fizzbuzz

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,1 +1,8 @@
-main = print [if x `mod` 15 == 0 then "Fizzbuzz" else if x `mod` 5 == 0 then "Buzz" else if x `mod` 3 == 0 then "Fizz" else show x | x <- [1..100]]
+fizzbuzz :: (Integral a, Show a) => a -> [Char]
+fizzbuzz x
+    | x `mod` 15 == 0 = "Fizzbuzz"
+    | x `mod` 5 == 0  = "Buzz"
+    | x `mod` 3 == 0  = "Fizz"
+    | otherwise       = show x
+
+main = print [fizzbuzz x | x <- [1..100]]
