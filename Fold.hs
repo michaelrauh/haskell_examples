a1200a2dfdc202ab1a17bcd2ef017b377d5a9edb
michaelrauh <michaelrauh@ymail.com>
2019-01-17 20:46:02 -0500

    implement foldWord. This is unsafe if called with the last word in the corpus

--- Fold.hs
+++ Fold.hs
@@ -1,13 +1,24 @@
 import Data.List
 
+example = "this is a string with repeated words. The string is long"
+type Answer = (String, String, String, String, String)
+
 adjacentWords :: Int -> String -> String -> [String]
 adjacentWords offset corpus word =
   let wordList = words corpus
       indices = elemIndices word wordList
   in map(\x -> wordList !! (x + offset)) indices
 
-nextWords :: String -> String -> [String]
-nextWords = adjacentWords 1
+nextWords :: String -> [String]
+nextWords = adjacentWords 1 example
 
-prevWords :: String -> String -> [String]
-prevWords = adjacentWords (-1)
+prevWords :: String -> [String]
+prevWords = adjacentWords (-1) example
+
+foldWord :: String -> [Answer]
+foldWord a = do
+      b <- nextWords a
+      d <- nextWords b
+      c <- prevWords d
+      a' <- prevWords c
+      return (a, b, d, c, a')
