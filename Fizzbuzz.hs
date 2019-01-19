3fa5592771ed34c91db86fcb079cd03881ce0ff0
michaelrauh <michaelrauh@ymail.com>
2019-01-19 12:28:31 -0500

    remove other small examples

--- Fizzbuzz.hs
+++
@@ -1,8 +0,0 @@
-fizzbuzz :: (Integral a, Show a) => a -> [Char]
-fizzbuzz x
-    | x `mod` 15 == 0 = "Fizzbuzz"
-    | x `mod` 5 == 0  = "Buzz"
-    | x `mod` 3 == 0  = "Fizz"
-    | otherwise       = show x
-
-main = print [fizzbuzz x | x <- [1..100]]
