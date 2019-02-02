bc9de9c137da272f8d0a07ca4bf7442c9de72511
michaelrauh <michaelrauh@ymail.com>
2019-02-02 17:41:55 -0500

    fix test that is finding rotation of input

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,12 +2,11 @@ import Control.Exception (evaluate)
 import Test.Hspec
 import Test.QuickCheck
 import Fold2
-import Fold3
-import Fold4
+import FoldHorizontal
 import qualified Data.Matrix as M
 
 main :: IO ()
 main = hspec $ do
   describe "execute2" $ do
     it "folds a list of words into a formatted answer" $ do
-      execute2 ["a", "b", "c", "d", "a", "c", "b", "d"] ["a", "b", "c", "d"] `shouldBe` [M.fromList 2 2 ["a","b","c","d"]]
+      execute2 ["a", "b", "c", "d", "a", "c", "b", "d"] ["a", "b", "c", "d"] `shouldBe` [M.fromList 2 2 ["a","b","c","d"], M.fromList 2 2 ["a","c","b","d"]]
