4444b93faaff7170d19979f95d7935fa94b24bdc
michaelrauh <michaelrauh@ymail.com>
2019-03-03 13:46:04 -0500

    delete untested code

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,6 @@
 module Main where
 
-import Common
-
-import Data.List
-import Control.Monad
-import Fold2
-import qualified Data.Matrix as M
-import FoldHorizontal
-import FoldVertical
-
 main :: IO ()
 main = do
   contents <- getContents
-  let wordList = words contents
-      uniqueWords = rmdups wordList
-      answer2 = execute2 wordList uniqueWords
-      answer32 = executeHorizontal wordList answer2
-      answer33 = executeVertical wordList answer32
-  putStrLn "2x2 results:\n\n"
-  putStr $ show answer2
-  putStrLn "3x2 results:\n\n"
-  putStr $ show answer32
-  putStrLn "3x3 results:\n\n"
-  putStr $ show answer33
+  putStr contents
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,25 +1,11 @@
 import Control.Exception (evaluate)
 import Test.Hspec
 import Test.QuickCheck
-import Fold2
-import FoldHorizontal
-import qualified Data.Matrix as M
 
 {-# ANN module "HLint: ignore Redundant do" #-}
 
 main :: IO ()
 main = hspec $ do
-  describe "execute2" $ do
-    it "folds a list of words into a formatted answer" $ do
-      let expected2x2 = [M.fromList 2 2 ["a","b","c","d"], M.fromList 2 2 ["a","c","b","d"]]
-          answers2x2 = execute2 ["a", "b", "c", "d", "a", "c", "b", "d"] ["a", "b", "c", "d"]
-      answers2x2 `shouldBe` expected2x2
-      
-  describe "executeHorizontal" $ do
-    it "combines answers horizontally to form wider answers" $ do
-      --  a b     b c      a b c
-      --  d e  +  e f  ->  d e f
-      let inputCorpus = ["a", "b", "c", "d", "e", "f", "a", "d", "b", "e", "c", "f"]
-          inputMatrices = [M.fromList 2 2 ["a", "b", "d", "e"], M.fromList 2 2 ["b", "c", "e", "f"]]
-          expectedMatrices = [M.fromList 2 3 ["a", "b", "c", "d", "e", "f"]]
-      executeHorizontal inputCorpus inputMatrices `shouldBe` expectedMatrices
+  describe "failure" $ do
+    it "fails" $ do
+      True `shouldBe` False
