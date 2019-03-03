e4004aca37ce2aea3a4ab66c826a6bef211d6610
michaelrauh <michaelrauh@ymail.com>
2019-03-03 15:58:20 -0500

    build reverse map by reversing each tuple

--- src/MapBuilder.hs
+++ src/MapBuilder.hs
@@ -12,10 +12,13 @@ buildNextWordMap :: Ord a => [a] -> Map.Map a (S.Set a)
 buildNextWordMap wordList = Map.fromListWith S.union $ buildSlidingTuple wordList
 
 buildPreviousWordMap :: Ord a => [a] -> Map.Map a (S.Set a)
-buildPreviousWordMap wordList = Map.fromListWith S.union $ buildSlidingTuple $ reverse wordList
+buildPreviousWordMap wordList = Map.fromListWith S.union $ map reverseSingletonTuple $ buildSlidingTuple wordList
 
 buildSlidingTuple :: [a] -> [(a, S.Set a)]
 buildSlidingTuple [] = []
 buildSlidingTuple [first] = []
 buildSlidingTuple [first, second] = [(first, S.singleton second)]
 buildSlidingTuple (first:second:rest) = (first, S.singleton second) : buildSlidingTuple (second : rest)
+
+reverseSingletonTuple :: (a1, S.Set a2) -> (a2, S.Set a1)
+reverseSingletonTuple (first, second) = (head $ S.elems second, S.singleton first)
