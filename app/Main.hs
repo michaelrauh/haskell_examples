71ac18f834ccc08576e33266029e34aaadf833db
michaelrauh <michaelrauh@ymail.com>
2019-03-03 13:37:33 -0500

    failing execute horizontal test shows that there is some flaw in the logic

--- app/Main.hs
+++ app/Main.hs
@@ -13,14 +13,13 @@ main :: IO ()
 main = do
   contents <- getContents
   let wordList = words contents
-  let uniqueWords = rmdups wordList
-  let answer2 = execute2 wordList uniqueWords
-  let answer32 = executeHorizontal wordList answer2
-  let answer33 = executeVertical wordList answer32
+      uniqueWords = rmdups wordList
+      answer2 = execute2 wordList uniqueWords
+      answer32 = executeHorizontal wordList answer2
+      answer33 = executeVertical wordList answer32
   putStrLn "2x2 results:\n\n"
   putStr $ show answer2
   putStrLn "3x2 results:\n\n"
   putStr $ show answer32
   putStrLn "3x3 results:\n\n"
   putStr $ show answer33
-  
