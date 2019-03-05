5b65b314dd2be13454331805afa47ceec2dd1a96
michaelrauh <michaelrauh@ymail.com>
2019-03-04 21:53:13 -0500

    quick implementation for main showing 3x3 solutions with 500 line input

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
 module Main where
 
+import Data.List
+import Control.Monad
+import FoldSquare
+import qualified Data.Matrix as M
+import CombineHorizontal
+import CombineVertical
+import MapBuilder
+
 main :: IO ()
 main = do
   contents <- getContents
-  putStr contents
+  let wordList = words contents
+      uniqueWords = nub wordList
+      answer2 = foldSquare wordList uniqueWords
+      phraseMap = buildPhraseMap wordList 2
+      answer32 = combineHorizontal phraseMap answer2
+      answer33 = combineVertical phraseMap answer32
+  putStrLn "2x2 results:\n\n"
+  putStr $ show answer2
+  putStrLn "3x2 results:\n\n"
+  putStr $ show answer32
+  putStrLn "3x3 results:\n\n"
+  putStr $ show answer33
