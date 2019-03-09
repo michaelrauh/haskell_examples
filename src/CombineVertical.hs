a44ffb78a78269c91657ed6ddbef0d201262bca1
michaelrauh <michaelrauh@ymail.com>
2019-03-09 17:16:32 -0500

    bubble matrix slicing

--- src/CombineVertical.hs
+++ src/CombineVertical.hs
@@ -13,11 +13,11 @@ import Control.Monad
 type MatrixPair = (Matrix, Matrix)
 type Matrix = M.Matrix String
 
-combineVertical = combine getBottomRowList (M.<->)
+combineVertical = combine getBottomRowList (M.<->) getColumns
 
-combine getEdgeOfMatrix matrixCombineOperator phraseMap inputMatrices =
+combine getEdgeOfMatrix matrixCombineOperator matrixSlicingOperator phraseMap inputMatrices =
   let possiblePairs = findPossiblePairs inputMatrices
-      answers = filterPairs getEdgeOfMatrix possiblePairs phraseMap
+      answers = filterPairs getEdgeOfMatrix matrixSlicingOperator possiblePairs phraseMap
       final = map (combineMatrixPair matrixCombineOperator) answers
   in nub final
 
@@ -37,22 +37,21 @@ centersOverlapVertically (top, bottom) =
 wordInList :: (String, [String]) -> Bool
 wordInList (target, possibilities) = target `elem` possibilities
 
-filterFoldable getEdgeOfMatrix phraseMap (first, second) =
+filterFoldable getEdgeOfMatrix matrixSlicingOperator phraseMap (first, second) =
   let nextWords' = nextWords phraseMap
-      froms = getFroms first
+      froms = matrixSlicingOperator first
       possibilities = getPossibilities froms phraseMap
       correspondences = getZips getEdgeOfMatrix possibilities second
       answers = getAnswers correspondences
   in checkAnswers answers
 
-getFroms = getColumns
 getPossibilities m phraseMap = map (nextWords phraseMap) m
 getZips getEdgeOfMatrix possibilities m = zip (getEdgeOfMatrix m) possibilities
 getAnswers = map wordInList
 checkAnswers = and
 
-filterPairs getEdgeOfMatrix matrixPairs phraseMap =
+filterPairs getEdgeOfMatrix matrixSlicingOperator matrixPairs phraseMap =
   let candidates = filter filterCandidates matrixPairs
-  in  filter (filterFoldable getEdgeOfMatrix phraseMap) candidates
+  in  filter (filterFoldable getEdgeOfMatrix matrixSlicingOperator phraseMap) candidates
 
 combineMatrixPair matrixCombineOperator (first, second) = first `matrixCombineOperator` getBottomRow second
