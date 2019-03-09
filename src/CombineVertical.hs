74bbe5fe9b2a8b32b481d571847304695ec8e432
michaelrauh <michaelrauh@ymail.com>
2019-03-09 17:04:46 -0500

    bubble get edge of matrix

--- src/CombineVertical.hs
+++ src/CombineVertical.hs
@@ -13,11 +13,11 @@ import Control.Monad
 type MatrixPair = (Matrix, Matrix)
 type Matrix = M.Matrix String
 
-combineVertical = combine (M.<->)
+combineVertical = combine getBottomRowList (M.<->)
 
-combine matrixCombineOperator phraseMap inputMatrices =
+combine getEdgeOfMatrix matrixCombineOperator phraseMap inputMatrices =
   let possiblePairs = findPossiblePairs inputMatrices
-      answers = filterPairs possiblePairs phraseMap
+      answers = filterPairs getEdgeOfMatrix possiblePairs phraseMap
       final = map (combineMatrixPair matrixCombineOperator) answers
   in nub final
 
@@ -37,22 +37,22 @@ centersOverlapVertically (top, bottom) =
 wordInList :: (String, [String]) -> Bool
 wordInList (target, possibilities) = target `elem` possibilities
 
-filterFoldable phraseMap (first, second) =
+filterFoldable getEdgeOfMatrix phraseMap (first, second) =
   let nextWords' = nextWords phraseMap
       froms = getFroms first
       possibilities = getPossibilities froms phraseMap
-      correspondences = getZips possibilities second
+      correspondences = getZips getEdgeOfMatrix possibilities second
       answers = getAnswers correspondences
   in checkAnswers answers
 
 getFroms = getColumns
 getPossibilities m phraseMap = map (nextWords phraseMap) m
-getZips possibilities m = zip (getBottomRowList m) possibilities
+getZips getEdgeOfMatrix possibilities m = zip (getEdgeOfMatrix m) possibilities
 getAnswers = map wordInList
 checkAnswers = and
 
-filterPairs matrixPairs phraseMap =
+filterPairs getEdgeOfMatrix matrixPairs phraseMap =
   let candidates = filter filterCandidates matrixPairs
-  in  filter (filterFoldable phraseMap) candidates
+  in  filter (filterFoldable getEdgeOfMatrix phraseMap) candidates
 
 combineMatrixPair matrixCombineOperator (first, second) = first `matrixCombineOperator` getBottomRow second
