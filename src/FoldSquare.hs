72c4e6f24e2a7b63f4660ddf6053feff52204529
michaelrauh <michaelrauh@ymail.com>
2019-03-14 20:54:14 -0400

    use types

--- src/FoldSquare.hs
+++ src/FoldSquare.hs
@@ -1,6 +1,4 @@
-module FoldSquare
-    ( foldSquare
-    ) where
+module FoldSquare (foldSquare) where
 
 import Data.List
 import qualified Data.Matrix as M
@@ -10,17 +8,17 @@ import MapBuilder
 import MatrixUtils
 
 type Answer = (String, String, String, String, String)
-type FormattedAnswer = (String, String, String, String)
 type StringMatrix = M.Matrix String
+type MapToSet = Map.Map String (S.Set String)
 
-foldSquare :: Foldable t => [String] -> t String -> [M.Matrix String]
+foldSquare :: [String] -> [String] -> [M.Matrix String]
 foldSquare wordList uniqueWords =
   let nextMap = buildNextWordMap wordList
       prevMap = buildPreviousWordMap wordList
       answers = concatMap (filterFoldedWords . foldWord nextMap prevMap) uniqueWords
   in removeEquivalentMatrices $ map matrixfy answers
 
-foldWord :: Ord e => Map.Map e (S.Set e) -> Map.Map e (S.Set e) -> e -> [(e, e, e, e, e)]
+foldWord :: MapToSet -> MapToSet -> String -> [Answer]
 foldWord wordMap prevMap a =
   let nextWords' = nextWords wordMap
       prevWords = nextWords prevMap
@@ -35,7 +33,8 @@ foldWord wordMap prevMap a =
 filterFoldedWords :: [Answer] -> [Answer]
 filterFoldedWords = filter(\(a, b, _, c, a') -> b /= c && a == a')
 
+removeEquivalentMatrices :: [StringMatrix] -> [StringMatrix]
 removeEquivalentMatrices = map head . groupBy isTranspose
 
-matrixfy :: (a, a, a, a, e) -> M.Matrix a
+matrixfy :: Answer -> StringMatrix
 matrixfy (a, b, d, c, _) = M.fromList 2 2 [a, b, c, d]
