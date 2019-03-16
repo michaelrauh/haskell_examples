c80b39981ccc975b4592c57743692783e68f8915
michaelrauh <michaelrauh@ymail.com>
2019-03-16 19:31:00 -0400

    create high level sketch of functions for combine next dimension

--- src/CombineNextDimension.hs
+++ src/CombineNextDimension.hs
@@ -2,14 +2,21 @@ module CombineNextDimension (combineNextDimension, Box (Square, Hyper)) where
 import qualified Data.Matrix as M
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as S
+import Control.Applicative
 
 type Matrix = M.Matrix String
 data Box = Square Matrix | Hyper Box Box deriving (Show, Eq)
 type PhraseMap = Map.Map [String] (S.Set String)
 
 combineNextDimension :: PhraseMap -> [Box] -> [Box]
-combineNextDimension phraseMap boxList = []
+combineNextDimension phraseMap boxList =
+  let allPairs = liftA2 (,) boxList boxList
+      filtered = filterPairs allPairs
+      combined = map combinePairs filtered
+  in combined
 
--- combineNextDimension :: Box -> Box -> Box
--- combineNextDimension (Square x) (Square y) = Hyper (Square $ M.fromList 2 2 ["a", "b", "c", "d"]) (Square $ M.fromList 2 2 ["a", "b", "c", "d"])
--- combineNextDimension (Hyper x y) (Hyper a b) = Hyper (Hyper x y) (Hyper a b)
+combinePairs :: (Box, Box) -> Box
+combinePairs (m1, m2) = m1
+
+filterPairs :: [(Box, Box)] -> [(Box, Box)]
+filterPairs boxList = boxList
