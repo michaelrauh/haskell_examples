f4283db095274a738e52f75fb8b5328656784f0b
michaelrauh <michaelrauh@ymail.com>
2019-03-24 15:47:50 -0400

    strip out everything except for map builder

--- app/Main.hs
+++ app/Main.hs
@@ -2,23 +2,10 @@ module Main where
 
 import Data.List
 import Control.Monad
-import FoldSquare
 import qualified Data.Matrix as M
-import Combine
 import MapBuilder
 
 main :: IO ()
 main = do
   contents <- getContents
-  let wordList = words contents
-      uniqueWords = nub wordList
-      answer2 = foldSquare wordList uniqueWords
-      phraseMap = buildPhraseMap wordList 2
-      answer32 = combineHorizontal phraseMap answer2
-      answer33 = combineVertical phraseMap answer32
-  putStrLn "2x2 results:\n\n"
-  putStr $ show answer2
-  putStrLn "3x2 results:\n\n"
-  putStr $ show answer32
-  putStrLn "3x3 results:\n\n"
-  putStr $ show answer33
+  putStr contents
