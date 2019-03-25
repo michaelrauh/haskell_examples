cef44f065a6bdb05fedba3fccada0f2d823e3f0f
michaelrauh <michaelrauh@ymail.com>
2019-03-25 08:40:24 -0400

    make a box type that tracks the top right corner. Currently unused

--- src/Tree.hs
+++ src/Tree.hs
@@ -3,6 +3,9 @@ module Tree (
             upDimension) where
 
 data Orthotope = Point String | Orthotope [Orthotope] deriving (Show, Eq)
+data Box = Box { getOrthotope :: Orthotope,
+                 getTopRightCorner :: String
+                }
 
 upDimension :: Orthotope -> Orthotope -> Orthotope
 upDimension a b = Orthotope [a, b]
