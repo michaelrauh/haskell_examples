f5971bf8f06f86f72a309c0b42aae50a6f0f1009
michaelrauh <michaelrauh@ymail.com>
2019-03-28 21:50:27 -0400

    add tracking of right column

--- test/TreeSpec.hs
+++ test/TreeSpec.hs
@@ -28,14 +28,14 @@ spec = do
     describe "from string" $ do
       it "makes a box from a string" $ do
         let input = "foo"
-            expected = Box (Point "foo") "foo" "foo"
+            expected = Box (Point "foo") "foo" "foo" (Point "foo")
         fromString input `shouldBe` expected
 
     describe "upBoxDimension" $ do
       it "combines two boxes cross-dimensionally" $ do
         let firstOrtho = Orthotope [Orthotope [Point "foo", Point "bar"], Orthotope [Point "baz", Point "bang"]]
             secondOrtho = Orthotope [Orthotope [Point "sas", Point "quatch"], Orthotope [Point "is", Point "real"]]
-            firstBox = Box firstOrtho "foo" "bang"
-            secondBox = Box secondOrtho "sas" "real"
-            expected = Box (Orthotope [firstOrtho, secondOrtho]) "foo" "real"
+            firstBox = Box firstOrtho "foo" "bang" $ Orthotope [Point "bar", Point "bang"]
+            secondBox = Box secondOrtho "sas" "real" $ Orthotope [Point "quatch", Point "real"]
+            expected = Box (Orthotope [firstOrtho, secondOrtho]) "foo" "real" $ Orthotope [Orthotope [Point "bar", Point "bang"], Orthotope [Point "quatch", Point "real"]]
         upBoxDimension firstBox secondBox `shouldBe` expected
