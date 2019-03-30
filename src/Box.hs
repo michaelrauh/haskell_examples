99936834a8c16fe21e479ba6f4b87c51147c81ef
michaelrauh <michaelrauh@ymail.com>
2019-03-30 18:40:54 -0400

    remove unneeded function, remove todo

--- src/Box.hs
+++ src/Box.hs
@@ -12,7 +12,7 @@ data Box = Box { getOrthotope :: O.Orthotope,
 
 
 upDimension :: Box -> Box -> Box
-upDimension (Box o1 bl1 tr1 rc1) (Box o2 bl2 tr2 rc2) = Box (O.upDimension o1 o2) bl1 tr2 (O.findRightCol rc1 rc2)
+upDimension (Box o1 bl1 tr1 rc1) (Box o2 bl2 tr2 rc2) = Box (O.upDimension o1 o2) bl1 tr2 (O.upDimension rc1 rc2)
 
 fromStringPair :: (String, String) -> Box
 fromStringPair (f, s) = Box (O.Orthotope [O.Point f, O.Point s]) f s (O.Point s)
