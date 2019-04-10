7df4c8ddccdbeb252844f10c44ac406485d4dbcc
michaelrauh <michaelrauh@ymail.com>
2019-04-10 19:47:21 -0400

    add tracking of first branch last leaf, as it will end up being the top right corner in the event of an add length operation

--- src/BoxData.hs
+++ src/BoxData.hs
@@ -5,5 +5,6 @@ data Box = Box { getOrthotope :: Orthotope String,
                  getBottomLeftCorner :: String,
                  getTopRightCorner :: String,
                  getRightColumn :: Orthotope String,
-                 getLines :: Orthotope String
+                 getLines :: Orthotope String,
+                 getFirstBranchLastLeaf :: String
                 } deriving (Show, Eq)
