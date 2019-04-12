a8a92e92b2cde19331a636e709e162760ddbabbd
michaelrauh <michaelrauh@ymail.com>
2019-04-11 20:22:01 -0400

    remove first branch last leaf

--- src/BoxData.hs
+++ src/BoxData.hs
@@ -5,6 +5,5 @@ data Box = Box { getOrthotope :: Orthotope String,
                  getBottomLeftCorner :: String,
                  getTopRightCorner :: String,
                  getRightColumn :: Orthotope String,
-                 getLines :: Orthotope String,
-                 getFirstBranchLastLeaf :: String
+                 getLines :: Orthotope String
                 } deriving (Show, Eq)
--- test/BoxSpec.hs
+++ test/BoxSpec.hs
@@ -16,14 +16,14 @@ spec = do
     describe "from string pair" $ do
       it "makes a box from a string pair" $ do
         let input = ("foo", "bar")
-            expected = D.Box (O.Orthotope [O.Point "foo", O.Point "bar"]) "foo" "bar" (O.Point "bar") (O.Point "foobar") "bar"
+            expected = D.Box (O.Orthotope [O.Point "foo", O.Point "bar"]) "foo" "bar" (O.Point "bar") (O.Point "foobar")
         B.fromStringPair input `shouldBe` expected
 
     describe "upDimension" $ do
       it "combines two boxes cross-dimensionally" $ do
         let firstOrtho = O.Orthotope [O.Orthotope [O.Point "foo", O.Point "bar"], O.Orthotope [O.Point "baz", O.Point "bang"]]
             secondOrtho = O.Orthotope [O.Orthotope [O.Point "sas", O.Point "quatch"], O.Orthotope [O.Point "is", O.Point "real"]]
-            firstBox = D.Box firstOrtho "foo" "bang" (O.Orthotope [O.Point "bar", O.Point "bang"]) (O.Orthotope [O.Point "foobar", O.Point "bazbang"]) "bar"
-            secondBox = D.Box secondOrtho "sas" "real" (O.Orthotope [O.Point "quatch", O.Point "real"]) (O.Orthotope [O.Point "sasquatch", O.Point "isreal"]) "quatch"
-            expected = D.Box (O.Orthotope [firstOrtho, secondOrtho]) "foo" "real" (O.Orthotope [O.Orthotope [O.Point "bar", O.Point "bang"], O.Orthotope [O.Point "quatch", O.Point "real"]]) (O.Orthotope [O.Orthotope [O.Point "foobar", O.Point "bazbang"], O.Orthotope [O.Point "sasquatch", O.Point "isreal"]]) "bar"
+            firstBox = D.Box firstOrtho "foo" "bang" (O.Orthotope [O.Point "bar", O.Point "bang"]) (O.Orthotope [O.Point "foobar", O.Point "bazbang"])
+            secondBox = D.Box secondOrtho "sas" "real" (O.Orthotope [O.Point "quatch", O.Point "real"]) (O.Orthotope [O.Point "sasquatch", O.Point "isreal"])
+            expected = D.Box (O.Orthotope [firstOrtho, secondOrtho]) "foo" "real" (O.Orthotope [O.Orthotope [O.Point "bar", O.Point "bang"], O.Orthotope [O.Point "quatch", O.Point "real"]]) (O.Orthotope [O.Orthotope [O.Point "foobar", O.Point "bazbang"], O.Orthotope [O.Point "sasquatch", O.Point "isreal"]])
         B.upDimension firstBox secondBox `shouldBe` expected
