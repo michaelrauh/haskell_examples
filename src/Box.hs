d5b56e0c99da6f6531508189bbafc16c9fb2d877
michaelrauh <michaelrauh@ymail.com>
2019-04-20 09:42:31 -0400

    remove addlength for now to focus on upDimension

--- src/Box.hs
+++ src/Box.hs
@@ -1,7 +1,6 @@
 module Box (
             fromStringPair,
             upDimension,
-            addLength,
             getCenter1,
             getCenter2,
             cornersDoNotMatch,
@@ -47,9 +46,6 @@ getCenter2 (Box (O.Orthotope ol) _ _ _ _) = head ol
 
 upDimension :: Box -> Box -> Box
 upDimension (Box o1 bl1 tr1 l1 c1) (Box o2 bl2 tr2 l2 c2) = Box (O.upDimension o1 o2) bl1 tr2 (O.zipConcat o1 o2) o2
-
-addLength :: Box -> Box -> Box
-addLength (Box o1@(O.Orthotope ol1) bl1 tr1 l1 c1) (Box o2 bl2 tr2 l2 c2) = Box (O.addLength o1 o2) bl1 tr2 (O.zipConcat (head ol1) l2) c2
 
 fromStringPair :: (String, String) -> Box
 fromStringPair (f, s) = Box (O.Orthotope [O.Point f, O.Point s]) f s (O.Point (f ++ s)) (O.Point s)
--- test/BoxSpec.hs
+++ test/BoxSpec.hs
@@ -31,16 +31,6 @@ spec = do
             expected = D.Box (O.Orthotope [firstOrtho, secondOrtho]) "foo" "real" (O.Orthotope [O.Orthotope [O.Point "foosas", O.Point "barquatch"], O.Orthotope [O.Point "bazis", O.Point "bangreal"]]) secondOrtho
         B.upDimension firstBox secondBox `shouldBe` expected
 
-    describe "addLength" $ do
-      it "combines two boxes in the most recently added dimension" $ do
-        let firstOrtho = O.Orthotope [O.Orthotope [O.Point "foo", O.Point "bar"], O.Orthotope [O.Point "baz", O.Point "bang"]]
-            secondOrtho = O.Orthotope [O.Orthotope [O.Point "baz", O.Point "bang"], O.Orthotope [O.Point "is", O.Point "real"]]
-            resultOrtho = O.Orthotope [O.Orthotope [O.Point "foo", O.Point "bar"], O.Orthotope [O.Point "baz", O.Point "bang"], O.Orthotope [O.Point "is", O.Point "real"]]
-            firstBox = D.Box firstOrtho "foo" "bang" (O.Orthotope [O.Point "foobaz", O.Point "barbang"]) (O.Orthotope [O.Point "baz", O.Point "bang"])
-            secondBox = D.Box secondOrtho "sas" "real" (O.Orthotope [O.Point "bazis", O.Point "bangreal"]) (O.Orthotope [O.Point "is", O.Point "real"])
-            expected = D.Box resultOrtho "foo" "real" (O.Orthotope [O.Point "foobazis", O.Point "barbangreal"]) (O.Orthotope [O.Point "is", O.Point "real"])
-        B.addLength firstBox secondBox `shouldBe` expected
-
     describe "getCenter1" $ do
       it "is the same as the column being tracked for the box" $ do
         let firstOrtho = O.Orthotope [O.Orthotope [O.Point "foo", O.Point "bar"], O.Orthotope [O.Point "baz", O.Point "bang"]]
